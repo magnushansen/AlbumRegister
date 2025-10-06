ac9fe7a0687e27010614de81d6bb42a5fa42c127
magnushansen <magnushansen124@gmail.com>
2025-10-06 11:06:05 +0100

    view component added

---
+++ Views/Components/AlbumFeatured.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using AlbumRegister.Models;
+using AlbumRegister.Data;
+
+namespace AlbumRegister.Components;
+
+public class AlbumFeatured : ViewComponent
+{
+    private AlbumContext context;
+
+    public AlbumFeatured(AlbumContext ctx)
+    {
+        context = ctx;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var Albums = (IEnumerable<Album>)context.Albums;
+        Album a = Albums.OrderBy(x => Guid.NewGuid()).FirstOrDefault();
+        return View("/Views/Shared/_AlbumSummary.cshtml", a);
+    }
+}
